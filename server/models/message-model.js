e6d9b00f2b24242061eda46a39924989435e88a9
John Climie <johnclimie00@gmail.com>
2022-04-12 19:16:43 -0400

    Created models

---
+++ server/models/message-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const msgSchema = mongoose.Schema({
+    message: {
+        text: {
+            type: String,
+            required: true
+        },
+    },
+    users: Array,
+    userSender: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Msgs", msgSchema);
