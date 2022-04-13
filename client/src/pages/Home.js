43147faa898bf98afb5018d8f4fcba797c49a144
John Climie <johnclimie00@gmail.com>
2022-04-13 13:57:57 -0400

    Homepage redirects to login if not logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 import Userlist from '../components/Userlist';
 
 const Home = () => {
+    const nav = useNavigate()
+
+    useEffect(() => {if (!localStorage.getItem('loggedInUser')) {
+        nav('/');
+    }}, []);
 
     const [showMenu, setShowMenu] = useState(false);
 
