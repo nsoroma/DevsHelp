884db7549490e1865a7904cb0123897bce8655fc
John Climie <johnclimie00@gmail.com>
2022-04-15 13:44:31 -0400

    Revert "Revert "Revert "Back-end troubleshooting"""
    This reverts commit cc59b689285299b0bddbfd4e5a7ab2adefc458bf.

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 export const registerRoute = `/api/users/register`;
 export const loginRoute = '/api/users/login';
-export const allUsersRoute = '/api/users/getusers';
-export const sendMsgRoute = '/api/msgs/sendmsg';
-export const getMsgRoute = '/api/msgs/getmsg'
+export const allUsersRoute = 'api/users/getusers';
+export const sendMsgRoute = 'api/msgs/sendmsg';
+export const getMsgRoute = 'api/msgs/getmsg'
--- server/models/message-model.js
+++ server/models/message-model.js
@@ -1,23 +1,18 @@
 const mongoose = require('mongoose');
 
-const msgSchema = mongoose.Schema(
-    {
-        message: {
-            text: {
-                type: String,
-                required: true
-            },
-        },
-        users: Array,
-        userSender: {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Users',
+const msgSchema = mongoose.Schema({
+    message: {
+        text: {
+            type: String,
             required: true
         },
     },
-    {
-        timestamps: true,
+    users: Array,
+    userSender: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Users',
+        required: true
     }
-)
+})
 
 module.exports = mongoose.model("Msgs", msgSchema);
