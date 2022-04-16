74aeaac7e12bd4a09dbe688d3f8999e88770c2e9
John Climie <johnclimie00@gmail.com>
2022-04-16 15:29:33 -0400

    deploying

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,8 +1,7 @@
 // Exports all api routes for users and messages
-const host = process.env.PORT
 
-export const registerRoute = `${host}/api/users/register`;
-export const loginRoute = `${host}/api/users/login`;
-export const allUsersRoute = `${host}/api/users/getusers`;
-export const sendMsgRoute = `${host}/api/msgs/sendmsg`;
-export const getMsgRoute = `${host}/api/msgs/getmsg`;
+export const registerRoute = `/api/users/register`;
+export const loginRoute = '/api/users/login';
+export const allUsersRoute = '/api/users/getusers';
+export const sendMsgRoute = '/api/msgs/sendmsg';
+export const getMsgRoute = '/api/msgs/getmsg'
